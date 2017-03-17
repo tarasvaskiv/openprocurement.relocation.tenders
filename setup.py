f9eda0f25958783df351649eb65f90f5c803e8da
Andrew Leitsius <leits@quintagroup.com>
2017-03-17 13:14:51 +0200

    Add .travis.yml and fix readme

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ entry_points = {
 setup(name='openprocurement.relocation.tenders',
       version=version,
       description="",
-      long_description=open("README.rst").read(),
+      long_description=open("README.md").read(),
       classifiers=[
         "Framework :: Pylons",
         "License :: OSI Approved :: Apache Software License",
